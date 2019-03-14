82e5adb3062320b72e9f8cbe700cab20336b2621
Ezeonyebuchi Emmanuel Chukwuma <26045664+emmanchuks@users.noreply.github.com>
2019-03-14 13:37:06 +0100

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,54 @@
+library(dplyr)
+## Data download and unzip 
+
+# string variables for file download
+fileName <- "UCIdata.zip"
+url <- "http://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+dir <- "UCI HAR Dataset"
+
+# File download verification. If file does not exist, download to working directory.
+if(!file.exists(fileName)){
+  download.file(url,fileName, mode = "wb") 
+}
+
+# File unzip verification. If the directory does not exist, unzip the downloaded file.
+if(!file.exists(dir)){
+  unzip("UCIdata.zip", files = NULL, exdir=".")
+}
+
+#Reading the training tables
+traindfx <- read.table("UCI HAR Dataset/train/X_train.txt", header = FALSE)
+traindfy <- read.table("UCI HAR Dataset/train/y_train.txt", header = FALSE)
+subjtraindf <- read.table("UCI HAR Dataset/train/subject_train.txt", header = FALSE)
+
+#Reading the testing tables
+testdfx <- read.table("UCI HAR Dataset/test/X_test.txt", header = FALSE)
+testdfy <- read.table("UCI HAR Dataset/test/y_test.txt", header = FALSE)
+subtestdf <- read.table("UCI HAR Dataset/test/subject_test.txt", header = FALSE)
+
+#Reading supportive metadata
+featuresdf <- read.table("UCI HAR Dataset/features.txt", header = FALSE)
+activitylabelsdf <- read.table(("./UCI HAR Dataset/activity_labels.txt"), header = FALSE)
+
+#Merging Data
+mergeddfx <- rbind(traindfx, testdfx)
+mergeddfy <- rbind(traindfy, testdfy)
+mergedSubs <- rbind(subjtraindf, subtestdf)
+
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+selected_var <- featuresdf[grep("mean\\(\\)|std\\(\\)", featuresdf[,2]),]
+mergeddfx <- mergeddfx[,selected_var[,1]]
+
+# 3. Uses descriptive activity names to name the activities in the data set
+colnames(mergeddfy) <- "activity"
+mergeddfy$activitylabelsdf <- factor(mergeddfy$activity, labels = as.character(activitylabelsdf[,2]))
+activitylabel <- mergeddfy[,-1]
+
+# 4. Appropriately labels the data set with descriptive variable names.
+colnames(mergeddfx) <- featuresdf[selected_var[,1],2]
+
+colnames(mergedSubs) <- "subject"
+
+total <- cbind(mergeddfx, mergeddfy)
+total_mean <- total %>% group_by(activitylabelsdf, subject) %>% summarize_each(funs(mean))
+write.table(total_mean, file = "tidydata.txt", row.names = FALSE, col.names = TRUE)
